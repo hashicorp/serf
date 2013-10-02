cc76a00d728d238d28b4fd26bcccfaea659c8260
Armon Dadgar <armon.dadgar@gmail.com>
2013-10-02 15:27:14 -0700

    Util methods for encode/decode

---
+++ serf/util.go
@@ -0,0 +1,24 @@
+package serf
+
+import (
+	"bytes"
+	"github.com/ugorji/go/codec"
+)
+
+// Decode reverses the encode operation on a byte slice input
+func decode(buf []byte, out interface{}) error {
+	r := bytes.NewBuffer(buf)
+	hd := codec.MsgpackHandle{}
+	dec := codec.NewDecoder(r, &hd)
+	return dec.Decode(out)
+}
+
+// Encode writes an encoded object to a new bytes buffer
+func encode(msgType int, in interface{}) (*bytes.Buffer, error) {
+	buf := bytes.NewBuffer(nil)
+	buf.WriteByte(uint8(msgType))
+	hd := codec.MsgpackHandle{}
+	enc := codec.NewEncoder(buf, &hd)
+	err := enc.Encode(in)
+	return buf, err
+}
