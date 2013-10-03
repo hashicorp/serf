5e49af9a7d078ba9b964870b2ce6b027fe11d8d9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-10-03 11:33:15 -0700

    cli: IsHelp

--- cli/cli.go
+++ cli/cli.go
@@ -1,5 +1,9 @@
 package cli
 
+import (
+	"sync"
+)
+
 // CommandFactory is a type of function that is a factory for commands.
 // We need a factory because we may need to setup some state on the
 // struct that implements the command itself.
@@ -11,9 +15,29 @@ type CLI struct {
 	Args     []string
 	Commands map[string]CommandFactory
 	Ui       Ui
+
+	once   sync.Once
+	isHelp bool
+}
+
+// IsHelp returns whether or not the help flag is present within the
+// arguments.
+func (c *CLI) IsHelp() bool {
+	c.once.Do(c.processArgs)
+	return c.isHelp
 }
 
 // Run runs the actual CLI based on the arguments given.
 func (c *CLI) Run() (int, error) {
 	return 0, nil
+}
+
+func (c *CLI) processArgs() {
+	for _, arg := range c.Args {
+		// If the arg is a help flag, then we saw that, but don't save it.
+		if arg == "-h" || arg == "--help" {
+			c.isHelp = true
+			continue
+		}
+	}
 }
--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,3 +1,27 @@
 package cli
 
+import (
+	"testing"
+)
 
+func TestCLIIsHelp(t *testing.T) {
+	testCases := []struct {
+		args   []string
+		isHelp bool
+	}{
+		{[]string{"foo", "-h"}, true},
+		{[]string{"foo", "--help"}, true},
+		{[]string{"foo", "-h", "bar"}, true},
+		{[]string{"foo", "bar"}, false},
+		{[]string{"-h", "bar"}, true},
+	}
+
+	for _, testCase := range testCases {
+		cli := &CLI{Args: testCase.args}
+		result := cli.IsHelp()
+
+		if result != testCase.isHelp {
+			t.Errorf("Expected '%#v'. Args: %#v", testCase.isHelp, testCase.args)
+		}
+	}
+}
