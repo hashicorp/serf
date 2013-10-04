6689b48bf6bc92e6f40c92567696717916274ef4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-10-03 22:21:32 -0700

    main: -v and --version show the version

--- main.go
+++ main.go
@@ -28,8 +28,21 @@ func realMain() int {
 		log.SetOutput(os.Stderr)
 	}
 
+	// Get the command line args. We shortcut "--version" and "-v" to
+	// just show the version.
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "-v" || arg == "--version" {
+			newArgs := make([]string, len(args)+1)
+			newArgs[0] = "version"
+			copy(newArgs[1:], args)
+			args = newArgs
+			break
+		}
+	}
+
 	cli := &cli.CLI{
-		Args:     os.Args[1:],
+		Args:     args,
 		Commands: Commands,
 		Ui:       &cli.BasicUi{Writer: os.Stdout},
 	}
