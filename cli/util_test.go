3bc78eed5dfc6c243c7c4dbf77f5ef25ffdd2667
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-10-08 11:24:17 -1000

    cli: Also gets getBindAddr

--- cli/util_test.go
+++ cli/util_test.go
@@ -2,8 +2,27 @@ package cli
 
 import (
 	"time"
+	"net"
+	"sync"
 )
+
+var bindLock sync.Mutex
+var bindNum byte = 10
 
 func yield() {
 	time.Sleep(5 * time.Millisecond)
+}
+
+// Returns an unused address for binding to for tests.
+func getBindAddr() net.IP {
+	bindLock.Lock()
+	defer bindLock.Unlock()
+
+	result := net.IPv4(127, 0, 0, bindNum)
+	bindNum++
+	if bindNum > 255 {
+		bindNum = 10
+	}
+
+	return result
 }
