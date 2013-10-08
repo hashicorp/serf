a316d95c473278492e686b41505b604ee2d68f75
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-10-08 09:58:07 -1000

    rpc: Ability to run server

--- rpc/server.go
+++ rpc/server.go
@@ -5,6 +5,8 @@ package rpc
 
 import (
 	"github.com/hashicorp/serf/serf"
+	"io"
+	"log"
 	"net"
 	"net/rpc"
 )
@@ -14,6 +16,9 @@ type Server struct {
 	rpcServer *rpc.Server
 }
 
+// NewServer creates a new RPC server for the given Serf instance
+// and listener. It does not start the RPC server. The RPC server
+// can be shut down by stopping the listener.
 func NewServer(serf *serf.Serf, l net.Listener) (*Server, error) {
 	rpcServer := rpc.NewServer()
 	err := rpcServer.RegisterName("Serf", &endpoint{
@@ -27,4 +32,23 @@ func NewServer(serf *serf.Serf, l net.Listener) (*Server, error) {
 		listener:  l,
 		rpcServer: rpcServer,
 	}, nil
+}
+
+// Run runs the server, blocking forever until the listener is closed.
+func (s *Server) Run() error {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Printf("[ERR] Error accepting RPC connection: %s", err)
+			return err
+		}
+
+		log.Printf("[DEBUG] Accepted connection: %s", conn.RemoteAddr())
+		go s.ServeConn(conn)
+	}
+}
+
+// ServeConn serves a single connection.
+func (s *Server) ServeConn(conn io.ReadWriteCloser) {
+	s.rpcServer.ServeConn(conn)
 }
