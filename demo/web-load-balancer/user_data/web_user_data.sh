7a6e528ac5a16418cf13503257e3deadf4cf89cb
Armon Dadgar <armon.dadgar@gmail.com>
2013-10-22 22:07:19 -0700

    demo: Update the user_data

--- demo/web-load-balancer/user_data/web_user_data.sh
+++ demo/web-load-balancer/user_data/web_user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NODE_SETUP_URL="https://raw.github.com/hashicorp/serf/master/demo/web-load-balancer/setup_web_server.sh?login=mitchellh&token=f489c8d8bdbd7dcc10d2dcb19c04ab0d"
 
-SERF_SETUP_URL="https://raw.github.com/hashicorp/serf/master/demo/web-load-balancer/setup_serf.sh?login=mitchellh&token=0f5c264420dc0cc74fc9a9b421cbddb1"
+SERF_SETUP_URL="https://raw.github.com/hashicorp/serf/master/demo/web-load-balancer/setup_serf.sh?login=armon&token=3dafd8a678e5f1d5a621bdf64274ad01"
 
 # Setup the node itself
 wget -O - $NODE_SETUP_URL | bash
