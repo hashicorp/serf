9139bd8879339e65b2f20f38a1a5edd087ad2f7d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-10-28 09:35:48 +0400

    command: add "-detailed" to `serf members` to see the protocol versions

--- command/members.go
+++ command/members.go
@@ -19,14 +19,18 @@ Usage: serf members [options]
 
 Options:
 
+  -detailed                 Additional information such as protocol verions
+                            will be shown.
   -rpc-addr=127.0.0.1:7373  RPC address of the Serf agent.
 `
 	return strings.TrimSpace(helpText)
 }
 
 func (c *MembersCommand) Run(args []string, ui cli.Ui) int {
+	var detailed bool
 	cmdFlags := flag.NewFlagSet("members", flag.ContinueOnError)
 	cmdFlags.Usage = func() { ui.Output(c.Help()) }
+	cmdFlags.BoolVar(&detailed, "detailed", false, "detailed output")
 	rpcAddr := RPCAddrFlag(cmdFlags)
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
@@ -48,6 +52,13 @@ func (c *MembersCommand) Run(args []string, ui cli.Ui) int {
 	for _, member := range members {
 		ui.Output(fmt.Sprintf("%s    %s    %s    %s",
 			member.Name, member.Addr, member.Status, member.Role))
+
+		if detailed {
+			ui.Output(fmt.Sprintf("    Protocol Version: %d",
+				member.DelegateCur))
+			ui.Output(fmt.Sprintf("    Available Protocol Range: [%d, %d]",
+				member.DelegateMin, member.DelegateMax))
+		}
 	}
 
 	return 0
