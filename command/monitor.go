e3e554ae81e6e004a3ecf3b37ea076ff9f2021b6
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-27 16:42:29 -0800

    command/monitor: Subscribe to stream events and print those as well

--- command/monitor.go
+++ command/monitor.go
@@ -54,19 +54,42 @@ func (c *MonitorCommand) Run(args []string) int {
 	}
 	defer client.Close()
 
-	eventCh := make(chan string)
-	handle, err := client.Monitor(logutils.LogLevel(logLevel), eventCh)
+	logCh := make(chan string, 16)
+	monHandle, err := client.Monitor(logutils.LogLevel(logLevel), logCh)
 	if err != nil {
 		c.Ui.Error(fmt.Sprintf("Error starting monitor: %s", err))
 		return 1
 	}
-	defer client.Stop(handle)
+	defer client.Stop(monHandle)
+
+	eventCh := make(chan map[string]interface{}, 16)
+	streamHandle, err := client.Stream("*", eventCh)
+	if err != nil {
+		c.Ui.Error(fmt.Sprintf("Error starting stream: %s", err))
+		return 1
+	}
+	defer client.Stop(streamHandle)
 
 	eventDoneCh := make(chan struct{})
 	go func() {
 		defer close(eventDoneCh)
-		for e := range eventCh {
-			c.Ui.Info(e)
+	OUTER:
+		for {
+			select {
+			case log := <-logCh:
+				if log == "" {
+					break OUTER
+				}
+				c.Ui.Info(log)
+			case event := <-eventCh:
+				if event == nil {
+					break OUTER
+				}
+				c.Ui.Info("Event Info:")
+				for key, val := range event {
+					c.Ui.Info(fmt.Sprintf("\t%s: %#v", key, val))
+				}
+			}
 		}
 
 		c.lock.Lock()
