e4fc282fa9e17a8372626b439d9c04b4b626f0c8
Armon Dadgar <armon.dadgar@gmail.com>
2013-12-03 12:17:20 -0800

    command/members: more tests

--- command/members_test.go
+++ command/members_test.go
@@ -27,3 +27,87 @@ func TestMembersCommandRun(t *testing.T) {
 		t.Fatalf("bad: %#v", ui.OutputWriter.String())
 	}
 }
+
+func TestMembersCommandRun_statusFilter(t *testing.T) {
+	a1 := testAgent(t)
+	defer a1.Shutdown()
+
+	ui := new(cli.MockUi)
+	c := &MembersCommand{Ui: ui}
+	args := []string{
+		"-rpc-addr=" + a1.RPCAddr,
+		"-status=a.*e",
+	}
+
+	code := c.Run(args)
+	if code != 0 {
+		t.Fatalf("bad: %d. %#v", code, ui.ErrorWriter.String())
+	}
+
+	if !strings.Contains(ui.OutputWriter.String(), a1.SerfConfig.NodeName) {
+		t.Fatalf("bad: %#v", ui.OutputWriter.String())
+	}
+}
+
+func TestMembersCommandRun_statusFilter_failed(t *testing.T) {
+	a1 := testAgent(t)
+	defer a1.Shutdown()
+
+	ui := new(cli.MockUi)
+	c := &MembersCommand{Ui: ui}
+	args := []string{
+		"-rpc-addr=" + a1.RPCAddr,
+		"-status=(fail|left)",
+	}
+
+	code := c.Run(args)
+	if code != 0 {
+		t.Fatalf("bad: %d. %#v", code, ui.ErrorWriter.String())
+	}
+
+	if strings.Contains(ui.OutputWriter.String(), a1.SerfConfig.NodeName) {
+		t.Fatalf("bad: %#v", ui.OutputWriter.String())
+	}
+}
+
+func TestMembersCommandRun_roleFilter(t *testing.T) {
+	a1 := testAgent(t)
+	defer a1.Shutdown()
+
+	ui := new(cli.MockUi)
+	c := &MembersCommand{Ui: ui}
+	args := []string{
+		"-rpc-addr=" + a1.RPCAddr,
+		"-role=test",
+	}
+
+	code := c.Run(args)
+	if code != 0 {
+		t.Fatalf("bad: %d. %#v", code, ui.ErrorWriter.String())
+	}
+
+	if !strings.Contains(ui.OutputWriter.String(), a1.SerfConfig.NodeName) {
+		t.Fatalf("bad: %#v", ui.OutputWriter.String())
+	}
+}
+
+func TestMembersCommandRun_roleFilter_failed(t *testing.T) {
+	a1 := testAgent(t)
+	defer a1.Shutdown()
+
+	ui := new(cli.MockUi)
+	c := &MembersCommand{Ui: ui}
+	args := []string{
+		"-rpc-addr=" + a1.RPCAddr,
+		"-role=primary",
+	}
+
+	code := c.Run(args)
+	if code != 0 {
+		t.Fatalf("bad: %d. %#v", code, ui.ErrorWriter.String())
+	}
+
+	if strings.Contains(ui.OutputWriter.String(), a1.SerfConfig.NodeName) {
+		t.Fatalf("bad: %#v", ui.OutputWriter.String())
+	}
+}
