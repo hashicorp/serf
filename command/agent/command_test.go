d65508d319e93fb3f072b07f44ef4426f68f1b68
Armon Dadgar <armon.dadgar@gmail.com>
2013-12-27 12:36:49 -0800

    Adding test for advertise addr

--- command/agent/command_test.go
+++ command/agent/command_test.go
@@ -1,6 +1,7 @@
 package agent
 
 import (
+	"bytes"
 	"github.com/hashicorp/serf/testutil"
 	"github.com/mitchellh/cli"
 	"log"
@@ -158,5 +159,61 @@ func TestCommandRun_joinFail(t *testing.T) {
 	code := c.Run(args)
 	if code == 0 {
 		t.Fatal("should fail")
+	}
+}
+
+func TestCommandRun_advertiseAddr(t *testing.T) {
+	doneCh := make(chan struct{})
+	shutdownCh := make(chan struct{})
+	defer func() {
+		close(shutdownCh)
+		<-doneCh
+	}()
+
+	c := &Command{
+		ShutdownCh: shutdownCh,
+		Ui:         new(cli.MockUi),
+	}
+
+	rpcAddr := getRPCAddr()
+	args := []string{
+		"-bind", testutil.GetBindAddr().String(),
+		"-rpc-addr", rpcAddr,
+		"-advertise", "127.0.0.10:12345",
+	}
+
+	go func() {
+		code := c.Run(args)
+		if code != 0 {
+			log.Printf("bad: %d", code)
+		}
+
+		close(doneCh)
+	}()
+
+	testutil.Yield()
+
+	client, err := NewRPCClient(rpcAddr)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer client.Close()
+
+	members, err := client.Members()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if len(members) != 1 {
+		t.Fatalf("bad: %#v", members)
+	}
+
+	// Check the addr and port is as advertised!
+	m := members[0]
+	if bytes.Compare(m.Addr, []byte{127, 0, 0, 10}) != 0 {
+		t.Fatalf("bad: %#v", m)
+	}
+	if m.Port != 12345 {
+		t.Fatalf("bad: %#v", m)
 	}
 }
