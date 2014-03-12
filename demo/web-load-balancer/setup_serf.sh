2405ddb0bd27539831bd500765b64c107f39ec4d
Armon Dadgar <armon.dadgar@gmail.com>
2014-03-12 10:58:31 -0700

    demo: Remove use of deprecated -role flag

--- demo/web-load-balancer/setup_serf.sh
+++ demo/web-load-balancer/setup_serf.sh
@@ -23,7 +23,7 @@ sudo mv serf /usr/local/bin/serf
 # The member join script is invoked when a member joins the Serf cluster.
 # Our join script simply adds the node to the load balancer.
 cat <<EOF >/tmp/join.sh
-if [ "x\${SERF_SELF_ROLE}" != "xlb" ]; then
+if [ "x\${SERF_TAG_ROLE}" != "xlb" ]; then
     echo "Not an lb. Ignoring member join."
     exit 0
 fi
@@ -46,7 +46,7 @@ chmod +x /usr/local/bin/serf_member_join.sh
 # The member leave script is invoked when a member leaves or fails out
 # of the serf cluster. Our script removes the node from the load balancer.
 cat <<EOF >/tmp/leave.sh
-if [ "x\${SERF_SELF_ROLE}" != "xlb" ]; then
+if [ "x\${SERF_TAG_ROLE}" != "xlb" ]; then
     echo "Not an lb. Ignoring member leave"
     exit 0
 fi
@@ -71,7 +71,7 @@ stop on runlevel [!2345]
 exec /usr/local/bin/serf agent \\
     -event-handler "member-join=/usr/local/bin/serf_member_join.sh" \\
     -event-handler "member-leave,member-failed=/usr/local/bin/serf_member_left.sh" \\
-    -role=${SERF_ROLE} >>/var/log/serf.log 2>&1
+    -tag role=${SERF_ROLE} >>/var/log/serf.log 2>&1
 EOF
 sudo mv /tmp/agent.conf /etc/init/serf.conf
 
