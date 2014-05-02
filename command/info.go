29c05118b5640d4ad7acb36094eb5de3fdb564c3
Armon Dadgar <armon.dadgar@gmail.com>
2014-05-02 11:43:03 -0700

    command/info: Support JSON output

--- command/info.go
+++ command/info.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/mitchellh/cli"
@@ -22,14 +23,21 @@ Usage: serf info [options]
 
 Options:
 
+  -format                  If provided, output is returned in the specified
+                           format. Valid formats are 'json', and 'text' (default)
+
   -rpc-addr=127.0.0.1:7373 RPC address of the Serf agent.
+
+  -rpc-auth=""             RPC auth token of the Serf agent.
 `
 	return strings.TrimSpace(helpText)
 }
 
 func (i *InfoCommand) Run(args []string) int {
+	var format string
 	cmdFlags := flag.NewFlagSet("info", flag.ContinueOnError)
 	cmdFlags.Usage = func() { i.Ui.Output(i.Help()) }
+	cmdFlags.StringVar(&format, "format", "text", "output format")
 	rpcAddr := RPCAddrFlag(cmdFlags)
 	rpcAuth := RPCAuthFlag(cmdFlags)
 	if err := cmdFlags.Parse(args); err != nil {
@@ -49,19 +57,38 @@ func (i *InfoCommand) Run(args []string) int {
 		return 1
 	}
 
+	output, err := formatOutput(StatsContainer(stats), format)
+	if err != nil {
+		i.Ui.Error(fmt.Sprintf("Encoding error: %s", err))
+		return 1
+	}
+
+	i.Ui.Output(string(output))
+	return 0
+}
+
+func (i *InfoCommand) Synopsis() string {
+	return "Provides debugging information for operators"
+}
+
+type StatsContainer map[string]map[string]string
+
+func (s StatsContainer) String() string {
+	var buf bytes.Buffer
+
 	// Get the keys in sorted order
-	keys := make([]string, 0, len(stats))
-	for key := range stats {
+	keys := make([]string, 0, len(s))
+	for key := range s {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 
 	// Iterate over each top-level key
 	for _, key := range keys {
-		i.Ui.Output(key + ":")
+		buf.WriteString(fmt.Sprintf(key + ":\n"))
 
 		// Sort the sub-keys
-		subvals := stats[key]
+		subvals := s[key]
 		subkeys := make([]string, 0, len(subvals))
 		for k := range subvals {
 			subkeys = append(subkeys, k)
@@ -71,12 +98,8 @@ func (i *InfoCommand) Run(args []string) int {
 		// Iterate over the subkeys
 		for _, subkey := range subkeys {
 			val := subvals[subkey]
-			i.Ui.Output(fmt.Sprintf("\t%s = %s", subkey, val))
+			buf.WriteString(fmt.Sprintf("\t%s = %s\n", subkey, val))
 		}
 	}
-	return 0
-}
-
-func (i *InfoCommand) Synopsis() string {
-	return "Provides debugging information for operators"
+	return buf.String()
 }
