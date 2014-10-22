2ad6c7f338b8477e7b2537c3666328d23bc981d5
Seth Vargo <sethvargo@gmail.com>
2014-10-22 15:59:16 -0400

    Use the correct version of Serf

--- website/config.rb
+++ website/config.rb
@@ -5,7 +5,7 @@
 set :base_url, "https://www.serfdom.io/"
 
 activate :hashicorp do |h|
-  h.version      = '0.7.1'
+  h.version      = '0.6.3'
   h.bintray_repo = 'mitchellh/serf'
   h.bintray_user = 'mitchellh'
   h.bintray_key  = ENV['BINTRAY_API_KEY']
