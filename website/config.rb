baa717b7a625cb0d00223adf2fb6e3aa5c1285ff
Seth Vargo <sethvargo@gmail.com>
2014-10-22 16:20:15 -0400

    Move helpers into config.rb, remove download helpers, no prefixes

--- website/config.rb
+++ website/config.rb
@@ -9,4 +9,20 @@ activate :hashicorp do |h|
   h.bintray_repo = 'mitchellh/serf'
   h.bintray_user = 'mitchellh'
   h.bintray_key  = ENV['BINTRAY_API_KEY']
+
+  # Currently, Serf builds are not prefixed with serf_*
+  h.bintray_prefixed = false
+end
+
+helpers do
+  # This helps by setting the "active" class for sidebar nav elements
+  # if the YAML frontmatter matches the expected value.
+  def sidebar_current(expected)
+    current = current_page.data.sidebar_current
+    if current.start_with?(expected)
+      return " class=\"active\""
+    else
+      return ""
+    end
+  end
 end
