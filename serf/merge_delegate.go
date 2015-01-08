cd843ca36e7130b5cc4fa76c699bd6279789d50d
stephensearles <stephen.searles@shipwire.com>
2015-01-08 14:20:16 -0800

    simplifying the panic safety for merge delegate

--- serf/merge_delegate.go
+++ serf/merge_delegate.go
@@ -15,24 +15,24 @@ type mergeDelegate struct {
 }
 
 func (m *mergeDelegate) NotifyMerge(nodes []*memberlist.Node) (cancel bool) {
-	if m.serf.config.Merge != nil {
-		members := make([]*Member, len(nodes))
-		for idx, n := range nodes {
-			members[idx] = &Member{
-				Name:        n.Name,
-				Addr:        net.IP(n.Addr),
-				Port:        n.Port,
-				Tags:        m.serf.decodeTags(n.Meta),
-				Status:      StatusNone,
-				ProtocolMin: n.PMin,
-				ProtocolMax: n.PMax,
-				ProtocolCur: n.PCur,
-				DelegateMin: n.DMin,
-				DelegateMax: n.DMax,
-				DelegateCur: n.DCur,
-			}
-		}
-		return m.serf.config.Merge.NotifyMerge(members)
+	if m.serf.config.Merge == nil {
+		return false
 	}
-	return false
+	members := make([]*Member, len(nodes))
+	for idx, n := range nodes {
+		members[idx] = &Member{
+			Name:        n.Name,
+			Addr:        net.IP(n.Addr),
+			Port:        n.Port,
+			Tags:        m.serf.decodeTags(n.Meta),
+			Status:      StatusNone,
+			ProtocolMin: n.PMin,
+			ProtocolMax: n.PMax,
+			ProtocolCur: n.PCur,
+			DelegateMin: n.DMin,
+			DelegateMax: n.DMax,
+			DelegateCur: n.DCur,
+		}
+	}
+	return m.serf.config.Merge.NotifyMerge(members)
 }
