3b715791b881b89ca0e0c134cfda84adc54e4f89
Derek Chiang <derekchiang93@gmail.com>
2015-03-18 18:09:46 -0400

    Make encoding work by making fields public

--- coordinate/client.go
+++ coordinate/client.go
@@ -18,19 +18,19 @@ const (
 )
 
 type Client struct {
-	coord Coordinate
-	err   float64
+	Coord Coordinate
+	Err   float64
 	// The unit of time used for the following fields is millisecond
-	adjustment        float64
+	Adjustment        float64
 	adjustment_index  uint      // index into adjustment window
 	adjustment_window []float64 // a rolling window that stores the differences between expected distances and real distances
 }
 
 func NewClient() *Client {
 	return &Client{
-		coord:             NewCoordinate(DIMENTION),
-		err:               VIVALDI_ERROR,
-		adjustment:        0,
+		Coord:             NewCoordinate(DIMENTION),
+		Err:               VIVALDI_ERROR,
+		Adjustment:        0,
 		adjustment_index:  0,
 		adjustment_window: make([]float64, ADJUSTMENT_WINDOW_SIZE),
 	}
@@ -38,28 +38,28 @@ func NewClient() *Client {
 
 func (self *Client) Update(other *Client, _rtt time.Duration) {
 	rtt := float64(_rtt.Nanoseconds()) / (1000 * 1000) // 1 millisecond = 1000 * 1000 nanoseconds
-	dist := self.coord.DistanceTo(other.coord)
-	weight := self.err / (self.err + other.err)
+	dist := self.Coord.DistanceTo(other.Coord)
+	weight := self.Err / (self.Err + other.Err)
 	err_calc := math.Abs(dist-rtt) / rtt
-	self.err = err_calc*VIVALDI_CE*weight + self.err*(1-VIVALDI_CE*weight)
-	if self.err > VIVALDI_ERROR {
-		self.err = VIVALDI_ERROR
+	self.Err = err_calc*VIVALDI_CE*weight + self.Err*(1-VIVALDI_CE*weight)
+	if self.Err > VIVALDI_ERROR {
+		self.Err = VIVALDI_ERROR
 	}
 	delta := VIVALDI_CC * weight
-	self.coord = self.coord.Add(self.coord.DirectionTo(other.coord).Mul(delta * (rtt - dist)))
+	self.Coord = self.Coord.Add(self.Coord.DirectionTo(other.Coord).Mul(delta * (rtt - dist)))
 	self.updateAdjustment(other, rtt)
 }
 
 func (self *Client) updateAdjustment(other *Client, rtt float64) {
-	self.adjustment_window[self.adjustment_index] = rtt - self.coord.DistanceTo(other.coord)
+	self.adjustment_window[self.adjustment_index] = rtt - self.Coord.DistanceTo(other.Coord)
 	self.adjustment_index = (self.adjustment_index + 1) % ADJUSTMENT_WINDOW_SIZE
 	tmp := 0.0
 	for _, n := range self.adjustment_window {
 		tmp += n
 	}
-	self.adjustment = tmp / (2.0 * ADJUSTMENT_WINDOW_SIZE)
+	self.Adjustment = tmp / (2.0 * ADJUSTMENT_WINDOW_SIZE)
 }
 
 func (self *Client) DistanceTo(other *Client) time.Duration {
-	return time.Duration(self.coord.DistanceTo(other.coord)+self.adjustment+other.adjustment) * time.Millisecond
+	return time.Duration(self.Coord.DistanceTo(other.Coord)+self.Adjustment+other.Adjustment) * time.Millisecond
 }
