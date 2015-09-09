9aacf784b1ae893394da7fe4ee7251c76acef9f4
Dale Hamel <dale.hamel@shopify.com>
2015-09-09 19:39:40 -0400

    Don't attempt to rejoin hosts

--- command/agent/srv.go
+++ command/agent/srv.go
@@ -42,25 +42,38 @@ func (m *AgentSRV) run() {
 
 	for {
 		// Format the host address
-		records := m.querySRV()
+		records := m.findSRV()
 
-		n, err := m.agent.Join(records, m.replay)
+		// Attempt the join only if there are new records
+		if len(records) > 0 {
+			n, err := m.agent.Join(records, m.replay)
 
-		// Attempt the join
-		if err != nil {
-			m.logger.Printf("[ERR] agent.srv: Failed to join: %v", err)
+			if err != nil {
+				m.logger.Printf("[ERR] agent.srv: Failed to join: %v", err)
+			}
+			if n > 0 {
+				m.logger.Printf("[INFO] agent.srv: Joined %d hosts", n)
+			}
 		}
-		if n > 0 {
-			m.logger.Printf("[INFO] agent.srv: Joined %d hosts", n)
-		}
 
+		// Sleep until it's time to poll again
 		time.Sleep(srvPollInterval)
 	}
 }
 
-// querySRV looks up the SRV records and returns a slice of all SRV records
-func (m *AgentSRV) querySRV() []string {
+// findSRV looks up the SRV records and returns a slice of all SRV records
+// that are not currently cluster members
+func (m *AgentSRV) findSRV() []string {
 	var hosts []string
+
+	// map the members so that we only do a single O(n) search through members
+	known_members := make(map[string]bool)
+	for _, v := range m.agent.Serf().Members() {
+		member := fmt.Sprintf("%s:%d", v.Addr.String(), v.Port)
+		known_members[member] = true
+	}
+
+	// Look up each SRV record and check if it's already in the cluster
 	for _, record := range m.srvrecords {
 		_, srvhosts, err := net.LookupSRV("", "", record)
 
@@ -68,9 +81,24 @@ func (m *AgentSRV) querySRV() []string {
 			m.logger.Printf("[ERR] agent.srv: Failed to poll for new hosts: %v", err)
 		}
 
+		// Filter each hosts in the SRV record
 		for _, host := range srvhosts {
-			addr := fmt.Sprintf("%s:%d", host.Target, host.Port)
-			hosts = append(hosts, addr)
+			// Find its addresses, as it's the only unique ID we can rely on
+			ipaddrs, err := net.LookupIP(host.Target)
+
+			if err != nil {
+				m.logger.Printf("[ERR] agent.srv: resolve SRV record %s to IP %v", host.Target, err)
+			}
+
+			// For each address the host has, check if it's already in the cluster
+			for _, ipaddr := range ipaddrs {
+				addr := fmt.Sprintf("%s:%d", ipaddr.String(), host.Port)
+				if _, known := known_members[addr]; !known {
+					// If the host is not already in the cluster,
+					// Add it to the list of hosts to try to join
+					hosts = append(hosts, addr)
+				}
+			}
 		}
 	}
 	return hosts
