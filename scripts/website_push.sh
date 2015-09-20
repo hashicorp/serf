71776542f6ea9af86b698cf26f7759ebf7faeaa2
Seth Vargo <sethvargo@gmail.com>
2015-09-20 14:06:32 -0400

    Use a faster middleman deploy

--- scripts/website_push.sh
+++ scripts/website_push.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
+# Set the tmpdir
+if [ -z "$TMPDIR" ]; then
+  TMPDIR="/tmp"
+fi
+
+# Create a temporary build dir and make sure we clean it up. For
+# debugging, comment out the trap line.
+DEPLOY=`mktemp -d $TMPDIR/serf-www-XXXXXX`
+trap "rm -rf $DEPLOY" INT TERM EXIT
+
 # Get the parent directory of where this script is.
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
+# Copy into tmpdir
+cp -R $DIR/website/ $DEPLOY/
+
 # Change into that directory
-cd $DIR
+pushd $DEPLOY &>/dev/null
 
-# If bootstrap is on, this is the first push
-if [ "${BOOTSTRAP}" != "" ]; then
-    heroku config:set BINTRAY_API_KEY=$BINTRAY_API_KEY
-    heroku config:add BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git
-fi
+# Ignore some stuff
+touch .gitignore
+echo ".sass-cache" >> .gitignore
+echo "build" >> .gitignore
 
-# Push the subtree (force)
-git push heroku `git subtree split --prefix website master`:master --force
+# Add everything
+git init -q .
+git add .
+git commit -q -m "Deploy by $USER"
+
+git remote add heroku git@heroku.com:serf-www.git
+git push -f heroku master
+
+# Go back to our root
+popd &>/dev/null
