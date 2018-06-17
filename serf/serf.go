e57492c242eb42ba3a13c1dbca5a9d5ac2dda3aa
Ben Cessa <ben@pixative.com>
2018-06-16 20:11:39 -0400

    Fix Leaked Go Routines
    Prevent to leak go routines when failing to create a new node instance

--- serf/serf.go
+++ serf/serf.go
@@ -263,6 +263,7 @@ func Create(conf *Config) (*Serf, error) {
 
 	// Check that the meta data length is okay
 	if len(serf.encodeTags(conf.Tags)) > memberlist.MetaMaxSize {
+		close(serf.shutdownCh)
 		return nil, fmt.Errorf("Encoded length of tags exceeds limit of %d bytes", memberlist.MetaMaxSize)
 	}
 
@@ -292,6 +293,7 @@ func Create(conf *Config) (*Serf, error) {
 	// the queries
 	outCh, err := newSerfQueries(serf, serf.logger, conf.EventCh, serf.shutdownCh)
 	if err != nil {
+		close(serf.shutdownCh)
 		return nil, fmt.Errorf("Failed to setup serf query handler: %v", err)
 	}
 	conf.EventCh = outCh
@@ -300,6 +302,7 @@ func Create(conf *Config) (*Serf, error) {
 	if !conf.DisableCoordinates {
 		serf.coordClient, err = coordinate.NewClient(coordinate.DefaultConfig())
 		if err != nil {
+			close(serf.shutdownCh)
 			return nil, fmt.Errorf("Failed to create coordinate client: %v", err)
 		}
 	}
@@ -317,6 +320,7 @@ func Create(conf *Config) (*Serf, error) {
 			conf.EventCh,
 			serf.shutdownCh)
 		if err != nil {
+			close(serf.shutdownCh)
 			return nil, fmt.Errorf("Failed to setup snapshot: %v", err)
 		}
 		serf.snapshotter = snap
@@ -393,6 +397,7 @@ func Create(conf *Config) (*Serf, error) {
 	// and failure detection for the Serf instance.
 	memberlist, err := memberlist.Create(conf.MemberlistConfig)
 	if err != nil {
+		close(serf.shutdownCh)
 		return nil, fmt.Errorf("Failed to create memberlist: %v", err)
 	}
 
