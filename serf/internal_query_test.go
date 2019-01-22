0e0c01c6b3b6402022361103ff29a8b7d87120d3
Hans Hasselberg <me@hans.io>
2019-01-22 21:07:59 +0100

    truncate key list response (#546)
    Truncate key list response so it fits into one message instead of failing to respond.

--- serf/internal_query_test.go
+++ serf/internal_query_test.go
@@ -3,6 +3,7 @@ package serf
 import (
 	"log"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -85,5 +86,76 @@ func TestSerfQueries_Conflict_SameName(t *testing.T) {
 	case <-outCh:
 		t.Fatalf("Should not passthrough query!")
 	case <-time.After(50 * time.Millisecond):
+	}
+}
+
+func TestSerfQueries_estimateMaxKeysInListKeyResponseFactor(t *testing.T) {
+	q := Query{id: 0, serf: &Serf{config: &Config{NodeName: "", QueryResponseSizeLimit: DefaultConfig().QueryResponseSizeLimit * 10}}}
+	resp := nodeKeyResponse{Keys: []string{}}
+	for i := 0; i <= q.serf.config.QueryResponseSizeLimit; i++ {
+		resp.Keys = append(resp.Keys, "LeJcrRIZsZ9tPYJZW7Xllg==")
+	}
+	found := 0
+	for i := len(resp.Keys); i >= 0; i-- {
+		buf, err := encodeMessage(messageKeyResponseType, resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		qresp := q.createResponse(buf)
+		raw, err := encodeMessage(messageQueryResponseType, qresp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err = q.checkResponseSize(raw); err != nil {
+			resp.Keys = resp.Keys[0:i]
+			continue
+		}
+		found = i
+		break
+	}
+	if found == 0 {
+		t.Fatal("Didn't find anything!")
+	}
+	t.Logf("max keys in response with %d bytes: %d", q.serf.config.QueryResponseSizeLimit, len(resp.Keys))
+	t.Logf("factor: %d", q.serf.config.QueryResponseSizeLimit/len(resp.Keys))
+}
+
+func TestSerfQueries_keyListResponseWithCorrectSize(t *testing.T) {
+	s := serfQueries{logger: log.New(os.Stderr, "", log.LstdFlags)}
+	q := Query{id: 0, serf: &Serf{config: &Config{NodeName: "", QueryResponseSizeLimit: 1024}}}
+	cases := []struct {
+		resp     nodeKeyResponse
+		expected int
+		hasMsg   bool
+	}{
+		{expected: 0, hasMsg: false, resp: nodeKeyResponse{}},
+		{expected: 1, hasMsg: false, resp: nodeKeyResponse{Keys: []string{"LeJcrRIZsZ9tPYJZW7Xllg=="}}},
+		// has 50 keys which makes the response bigger than 1024 bytes.
+		{expected: 36, hasMsg: true, resp: nodeKeyResponse{Keys: []string{
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+			"LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==", "LeJcrRIZsZ9tPYJZW7Xllg==",
+		}}},
+	}
+	for _, c := range cases {
+		r := c.resp
+		_, _, err := s.keyListResponseWithCorrectSize(&q, &r)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if len(r.Keys) != c.expected {
+			t.Errorf("Expected %d vs %d", c.expected, len(r.Keys))
+		}
+		if c.hasMsg && !strings.Contains(r.Message, "truncated") {
+			t.Error("truncation message should be set")
+		}
 	}
 }
